3a8ac4b104c7e0d8b7312afe2f6877ef161f23ee
VNM02 <92353375+VNM02@users.noreply.github.com>
2022-09-22 18:28:03 +0530

    Alert Messages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,44 @@ import './App.css';
 import Navbar from "./components/Navbar.js";
 import TextFrom from "./components/TextForm";
 import About from './components/About';
+import Alert from "./components/Alert.js" ;
 let name="Vaibhav Mahajan"
 
 
 function App() {
   const [ mode , setMode ]=useState('dark');
+  const[alert,setAlert]=useState(null);
+  const showAlert =(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
   const toggleMode=()=>{
-    if(mode=='light')
+    if(mode==='light')
     {
       setMode('dark')
+      showAlert("Dark mode enabled","success");
+      // document.body.style.backgroundColor='grey';
     }
     else
     {
       setMode('light')
+     // document.body.style.backgroundColor='light';
+      showAlert("Normal mode enabled","success")
     }
   }
+  
   return (
     <>
 <Navbar title="TextUtils" about="About" toggleMode={toggleMode} mode={mode}/>
+<Alert alert={alert}/>
 <div className="container my-3" >
-<About></About>    
-<TextFrom heading="Text Box" />
+{/* <About></About>     */}
+<TextFrom heading="Text Box" mode={mode} showAlert={showAlert} />
 </div>
 
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,16 +6,19 @@ export default function TextForm(props) {
 		console.log("Button Was Clicked")
 		let newText=text.toUpperCase();
 		setText(newText)
+		props.showAlert("Coverted to uppercase","success")
 	}
 	const ChangeToDown= ()=> {                               // On click changes the entered text tolowercase
 		console.log("Button Was Clicked")
 		let newText=text.toLowerCase();
 		setText(newText)
+		props.showAlert("Coverted to lowercase","success")
 	}
 	const ClearText= ()=> {                               // On click clears
 		console.log("Button Was Clicked")
 		let newText='';
 		setText(newText)
+		props.showAlert("Text has been cleared","success")
 	}
 	const TrimText= ()=> {                               // On click changes trims the text
 		console.log("Button Was Clicked")
@@ -24,22 +27,26 @@ export default function TextForm(props) {
 	}
 	const ChangedText= (event) =>{                          // Shows whatever text is entered in the text box
 		console.log("Changed");
-		setText(event.target.value)           // text=settext              // this implies that whatever text is entered in the textbox is saved in the text  variable
+		setText(event.target.value)           //  text=settext    // this implies that whatever text is entered in the textbox is saved in the text  variable
 	}
 	const CopyText=(event)=>{
 		console.log("Text Has Been Copied");
 		var t=document.getElementById("myBox");
 		t.select();
 		navigator.clipboard.writeText(t.value);
+		props.showAlert("Text copied to clipboard","success")
 	}
 	const [text, setText] = useState('Enter The Text Here');
   return (
 	<>
-	<div className="container">
+	<div className="container" >
 	 <h1>{props.heading}</h1>
   <div className="mb-3">
     {/* <label for="myBox">{props.heading}</label> */}
-    <textarea className="form-control" value={text} onChange={ChangedText} id="myBox" rows="8"></textarea>
+    <textarea className="form-control" value={text} style={{
+		backgroundColor: props.mode==='dark'?'grey':'light',
+
+	}} onChange={ChangedText} id="myBox" rows="8"></textarea>
   </div>
 	<button className="btn btn-primary mx-2" onClick={ChangeToUp}>Convert To UpperCase</button>
 	<button className="btn btn-primary mx-2" onClick={ChangeToDown}>Convert To LowerCase</button>
@@ -47,7 +54,7 @@ export default function TextForm(props) {
 	<button className="btn btn-primary mx-2" onClick={ClearText}>Clear the paragraph</button>
 	<button className="btn btn-primary mx-2" onClick={CopyText}>Copy The Paragraph</button>
 	</div>
-	<div className="container my-3">
+	<div className="container my-3" >
 		<h1>Text Summary</h1>
 		<p>The entered paragraph has {text.split(" ").length} words and {text.length} characters</p>    {/* /// {text.split()} == it is an array */}
 		<p>{0.008 * text.split(" ").length} are the minutes required to read the full text</p>
@@ -55,3 +62,11 @@ export default function TextForm(props) {
 	</>
   )
 }
+
+
+// // style={{
+// 		backgroundColor:props.mode==='dark'?'white':'dark',
+		// color:props.mode==='dark'?'white':'dark'
+
+
+// 	}}
